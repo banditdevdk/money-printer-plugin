9ca68f2816971bf7357b0fa0ee9ffa053bcf2df9
davidek-d <dajo1002@stud.ek.dk>
2025-12-02 22:51:24 +0100

    First commit. Plugin setup done in IntelliJ. Paper 1.20.1 build with gradle.

---
+++ src/main/java/io/github/banditdevdk/moneyprinterplugin/MoneyPrinterPlugin.java
@@ -0,0 +1,17 @@
+package io.github.banditdevdk.moneyprinterplugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MoneyPrinterPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
